d5ccb596309e86a6d0fe3aee1fcf52347b1fbc99
wilgai <lorimerwilgay23@gmail.com>
2022-01-05 15:10:13 -0400

    Add project files.

---
+++ Mastermind/Program.cs
@@ -0,0 +1,35 @@
+﻿using Mastermind.Resources;
+using System;
+using System.Collections.Generic;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var code = new List<CodePeg>();
+            code.Add(CodePeg.White);
+            code.Add(CodePeg.Blue);
+            code.Add(CodePeg.Green);
+            code.Add(CodePeg.Black);
+            MastermindGame m = new MastermindGame(code);
+
+            var result = new List<ResultPeg>();
+
+            var guess = new List<CodePeg>();
+            guess.Add(CodePeg.Blue);
+            guess.Add(CodePeg.Red);
+            guess.Add(CodePeg.Green);
+            guess.Add(CodePeg.Black);
+            result = m.GetHints(guess);
+            foreach(var res in result)
+            {
+                Console.WriteLine(res);
+            }
+
+
+        }
+    }
+}
---
+++ Mastermind/Resources/Mastermind.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mastermind.Resources
+{
+    public class MastermindGame
+    {
+        private readonly List<CodePeg> code;
+
+        public MastermindGame(List<CodePeg> code)
+        {
+            this.code = code;
+        }
+
+        public List<ResultPeg> GetHints(List<CodePeg> guess)
+        {
+            // Implement your code here.
+            var result = new List<ResultPeg>();
+
+            if (Enumerable.SequenceEqual(code, guess))
+            {
+               //You won the Game!!
+               //Code to let the user know he has won.
+            }
+            else
+            {
+                //Keep trying!!
+                CodePeg[] c = code.ToArray();
+                CodePeg[] ch = guess.ToArray();
+                for (int i = 0; i <= c.Length-1; i++)
+                {
+                    for (int j = 0; j <= ch.Length-1; j++)
+                    {
+                        if(c[i]==ch[j])
+                        {
+                            if(i==j)
+                            {
+                                //There are some matched in the same position.
+                                result.Add(ResultPeg.Black);
+                                break;
+                            }
+                            //They matched in different position.
+                            result.Add(ResultPeg.White);
+                             break;
+                        }
+                        else
+                        {
+                            //There is no matched
+                            //Code to indicate the user there was no match at all.
+                            
+                        }
+
+                    }
+
+                }
+                   
+                }
+            return result;
+        }
+    }
+
+    public enum CodePeg
+    {
+        Black,
+        Blue,
+        Green,
+        Red,
+        White,
+        Yellow,
+    }
+
+    public enum ResultPeg
+    {
+        Black,
+        White,
+    }
+}
